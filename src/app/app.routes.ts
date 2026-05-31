56076071fbd3b015ee943a5eba6cc330fb7e7f4b
Quang <quangvp99@gmail.com>
2026-05-31 17:36:43 +0700

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,7 +17,7 @@ export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'portfolio', loadChildren: () => import('@app/features/portfolio/portfolio.routes').then(m => m.PORTFOLIO_ROUTES) },
